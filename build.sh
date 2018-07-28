7a36814e145ad2233af37a8f007ab0d99a0a062e
Konstantin Müller <konstantin.mueller@yandex.com>
2018-07-28 17:34:55 +0200

    add manifest for chrome and use separate folders for firefox and chrome, adjust build.sh accordingly

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-npx webpack --entry ./build/Main.js --output ./dist/Main.min.js
-uglifycss ./src/style.css > ./dist/style.min.css
+npx webpack --entry ./build/Main.js --output ./dist-firefox/Main.min.js
+uglifycss ./src/style.css > ./dist-firefox/style.min.css
+
+cp ./dist-firefox/Main.min.js ./dist-chrome/
+cp ./dist-firefox/style.min.css ./dist-chrome/
+
 echo Done!
