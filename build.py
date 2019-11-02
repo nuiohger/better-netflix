5739d57458e37f236c073ecc736069ddaa19ad9e
Konstantin Müller <konstantin.mueller.dev@gmail.com>
2019-11-02 14:17:57 +0100

    fix build script for firefox add-on reviewers and update readme for reviewers

--- build.py
+++ build.py
@@ -16,9 +16,13 @@ chrome_dir = f"{build_dir}/dist-chrome"
 
 def main():
     args = parse_args()
+    if args.review_build:
+        review_build()
+        return
+
     if args.typescript:
         build_typescript()
-    run_webpack()
+    run_webpack(firefox_dir)
     build_firefox()
     build_chrome()
     if args.zip:
@@ -36,6 +40,12 @@ def parse_args():
     parser.add_argument(
         "-t", "--typescript", action="store_true", help="build typescript"
     )
+    parser.add_argument(
+        "-rb",
+        "--review-build",
+        action="store_true",
+        help="build the Add-on for reviewers",
+    )
     return parser.parse_args()
 
 
@@ -48,14 +58,14 @@ def build_typescript():
     )
 
 
-def run_webpack():
+def run_webpack(destination_dir: str):
     run_subprocess(
         "npx",
         "webpack",
         "--entry",
         f"{base_dir}/build/Main.js",
         "--output",
-        f"{firefox_dir}/Main.js",
+        f"{destination_dir}/Main.js",
         "--mode",
         "none",
         success_msg="Successfully created JavaScript code",
@@ -100,21 +110,25 @@ def create_source_code_zip():
     dirs = ["./build", "./dist-firefox", "./src"]
     ignored_file_extensions = ["zip"]
     ignored_dirs = ["dist-firefox", "dist-chrome"]
-    file_names = ["build.py", "README"]
+    file_names = ["build.py", "README", "tsconfig.json"]
+
     with zipfile.ZipFile(
         "build/source-code.zip", "w", zipfile.ZIP_DEFLATED
     ) as zip_file:
         for filename in file_names:
             zip_file.write(os.path.join(base_dir, filename), arcname=filename)
+
         for folder in dirs:
             for root, folders, files in os.walk(folder):
                 if any(ignored in root for ignored in ignored_dirs):
                     continue
+
                 for file in files:
                     if any(
                         file.endswith(ignored) for ignored in ignored_file_extensions
                     ):
                         continue
+
                     cur_file = os.path.join(root, file)
                     zip_file.write(os.path.abspath(cur_file), arcname=cur_file)
 
@@ -123,6 +137,18 @@ def replace_dir(new: str, old: str):
     if os.path.exists(old):
         shutil.rmtree(old)
     shutil.copytree(new, old)
+
+
+def review_build():
+    print(
+        "Building TypeScript. tsc is generating a JavaScript file for each TypeScript file..."
+    )
+    build_typescript()
+    print(
+        "Generated JavaScript files in build directory.\nThe JavaScript files are merged into one JavaScript file by webpack.\n"
+    )
+    run_webpack(".")
+    print("\nThe merged JavaScript file is 'Main.js' in the current directory.")
 
 
 if __name__ == "__main__":
