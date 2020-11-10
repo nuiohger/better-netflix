c71d8bb922f79cda4414f1e6fa82c5c7c15674e3
Konstantin Müller <konstantin.mueller.dev@gmail.com>
2020-11-10 20:09:28 +0100

    bugfix: prevent creating multiple random video buttons

--- src/Controller/MyListController.ts
+++ src/Controller/MyListController.ts
@@ -1,17 +1,19 @@
 module MyListController {
+    const btnClass = "bn_btn";
+
     export function randomVideo(): void {
-        if(location.href !== "https://www.netflix.com/browse/my-list") return;
+        if (location.href !== "https://www.netflix.com/browse/my-list" || document.querySelector(`.${btnClass}`)) return;
 
         const button = document.createElement("button");
         button.textContent = "Pick random video";
-        button.classList.add("bn_btn");
+        button.classList.add(btnClass);
         button.addEventListener("click", () => {
             const allVideos: NodeListOf<HTMLElement> = document.querySelectorAll(".rowContainer .slider-item");
-            if(allVideos.length <= 0) return;
+            if (allVideos.length <= 0) return;
 
             const randomContainer: HTMLElement = allVideos[Math.floor(Math.random() * allVideos.length + 1) - 1];
             const randomVideo: HTMLElement = randomContainer.querySelector("a");
-            randomVideo.scrollIntoView({behavior: "smooth", block: "center"});
+            randomVideo.scrollIntoView({ behavior: "smooth", block: "center" });
             randomContainer.classList.add("bn_border");
         });
 
