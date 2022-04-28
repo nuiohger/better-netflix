ce9d99b5111ac6cc8214c5a8c5d3ac499d76b3f6
nuiohger <80313516+nuiohger@users.noreply.github.com>
2022-04-28 12:44:40 +0900

    update

--- publish/utils.py
+++ publish/utils.py
@@ -4,7 +4,8 @@ import sys
 
 def run_subprocess(*args: str, success_msg: str = "") -> None:
     try:
-        subprocess.run(list(args), check=True)
+        print(" ".join(list(args)))
+        subprocess.run(list(args), check=True, shell=True)
     except subprocess.CalledProcessError:
         print(
             f"An error occurred while executing the following command: '{' '.join(args)}'"
